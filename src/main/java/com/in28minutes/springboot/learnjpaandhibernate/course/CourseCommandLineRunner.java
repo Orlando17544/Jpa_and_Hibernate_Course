5fd4fef1bc05487b79b3f3261bb9802f549058eb
Orlando <orcj17544@gmail.com>
2024-02-02 11:19:04 -0600

    Implemented SpringDataJpa

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -6,22 +6,31 @@ import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner {
 	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
-		repository.insert(new Course(3, "Learn DevOps Now!", "in28minutes"));
+		repository.save(new Course(1, "Learn AWS Now!", "in28minutes"));
+		repository.save(new Course(2, "Learn Azure Now!", "in28minutes"));
+		repository.save(new Course(3, "Learn DevOps Now!", "in28minutes"));
 		
-		repository.deleteById(1);
+		repository.deleteById(1l);
 		
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(3l));
+		
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+		
+		System.out.println(repository.findByAuthor("in28minutes"));
+		System.out.println(repository.findByAuthor(""));
+		
+		System.out.println(repository.findByName("Learn Azure Now!"));
 	}
 }
